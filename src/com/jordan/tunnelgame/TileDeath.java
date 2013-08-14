bb8aab409d448e8b278078377a530144f74db760
Jordan <j_haack$@yahoo.com>
2013-08-14 12:37:58 -0500

    fire anim

--- src/com/jordan/tunnelgame/TileDeath.java
+++ src/com/jordan/tunnelgame/TileDeath.java
@@ -7,22 +7,22 @@ public class TileDeath extends Tile {
         super(coord, id);
         switch(id)
         {
-            case '*': anim = new Anim(Assets.iFireTile,new int[]{0,1,2,3,4,5,6,7,8,9},5); break;
+            case '*': anim = new Anim(Assets.iFireTile,new int[]{0,1,2,3,4,5,6,7,8,9},10); break;
         }
     }
 
     @Override
     public void collision(Chaser chaser, CollisionType type) {
         switch (type) {
-            case TOP: basicTopCollision(chaser); chaser.dead = true;
+            case TOP: //basicTopCollision(chaser); chaser.dead = true;
                 break;
-            case LEFT: basicLeftCollision(chaser); chaser.dead = true;
+            case LEFT: basicLeftCollision(chaser); //chaser.dead = true;
                 break;
-            case RIGHT: basicRightCollision(chaser); chaser.dead = true;
+            case RIGHT: basicRightCollision(chaser); //chaser.dead = true;
                 break;
             case BOTTOM: basicBottomCollision(chaser);
                 break;
-            case IN:
+            case IN: chaser.dead = true;
                 break;
             case NONE:
                 break;
